c23128c2e64585812672a5f5a4ae75d6f8daf7bb
Sunghyun Jung <93960513+hyuuunn@users.noreply.github.com>
2021-12-27 21:45:29 +0900

    Delete database_mysql.js

--- database_mysql.js
+++
@@ -1,9 +0,0 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '960227',
-  database : 'ediary'
-});
-
-connection.connect();
